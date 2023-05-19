bce40cba06193cea0e7dd796c409d4bb44a93478
developerpyk <developerpyk@gmail.com>
2023-05-19 16:42:44 +0900

    React Router로 menu-page 간단구현하기

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div className="App">
+   <BrowserRouter>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to = "/">Home</Link>
+          </li>
+          <li>
+            <Link to = "/mypage">MyPage</Link>
+          </li>
+          <li>
+            <Link to = "/dashboard">Dashboard</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path = "/" element = {<Home />} />
+        <Route path = "/mypage" element = {<MyPage />} />
+        <Route path = "/dashboard" element = {<Dashboard />} />
+      </Routes>
     </div>
+   </BrowserRouter>
+  </div>
   );
+}
+
+function Home() {
+  return <h1>Home</h1>;
+}
+
+// MyPage 컴포넌트
+function MyPage() {
+  return <h1>MyPage</h1>;
+}
+
+// Dashboard 컴포넌트
+function Dashboard() {
+  return <h1>Dashboard</h1>;
 }
 
 export default App;
